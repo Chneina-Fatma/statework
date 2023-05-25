f8b5d157ab3d7bc941a26e090f6479e4b86264f8
chneina fatma <chneina.chneina@gmail.com>
2023-05-25 17:07:20 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useSyncExternalStore } from 'react'
+import "./App.css"
 
 function App() {
+  const [show, setshow] = useState(false)
+  const [Parents, setparents] = useState([
+    {
+     img1:"father.png",
+    name1:"My Father",
+    age1: 65,
+   },
+    {
+     img1:"mother.png",
+    name1:"My Mother",
+    age1: 64,
+   },])
+    const [Brodhers, setstudent] = useState([
+         {
+          img:"brahim.jpg",
+         name:"Brahim",
+         age: 29,
+        },
+         {
+          img:"mrwa.jpg",
+         name:"Marwa",
+         age: 30,
+        },
+         {
+          img:"med.jpg",
+         name:"Mohamed",
+         age: 31,
+        },
+         {
+          img:"meriem.jpg",
+         name:"Meriem",
+         age: 32,
+        },
+        ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<div className='click'><button className='btn' onClick={ ()=>setshow(!show) }><b>click here to show Parents</b></button></div>
+ {show? <div className='container'>
+{Parents.map((el)=> <div className='card'>
+        <img src={el.img1}/>
+        <h1>{el.name1}</h1>
+        <h2>{el.age1}</h2>
+</div>)}</div>: <div id='show'>my brodhers</div>}
+
+    <div className='container'>
+       {Brodhers.map((el)=> <div className='card'>
+        <img src={el.img}/>
+        <h1>{el.name}</h1>
+        <h2>{el.age}</h2>
+        </div>)}
     </div>
-  );
+   
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/component/profil.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function profil(Parents,brodhers) {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default profil
